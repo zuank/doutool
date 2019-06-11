7ae3a766861ea3c5fce2c0ec6a8e69b98d6bac07
王越豪 <wangyuehao@jiangduoduo.com>
2019-06-11 14:21:08 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'doutool',
+    created(){
+      this.$http.post('http://api.toysmodel.cn/Shop/index.php?s=/App/get_product_list').then(response => {
+        console.log(response)
+      })
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
